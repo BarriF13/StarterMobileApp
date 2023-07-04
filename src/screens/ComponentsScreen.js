53a9d8fbe9739b4a7badc5e0b75a103f055076a7
BarriF <barrifarjadnia@hollandandbarrett.com>
2023-07-04 11:31:28 +0100

    first commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+  const greeting="Bella";
+  const array = ["ass", 123]
+  const greetMe = <Text>mana mana doodoodoodoo</Text>
+
+  return  <View>
+      <Text style={styles.textStyle}>Getting started with REACT NATIVE</Text>
+      <Text style={styles.zapataStyle}>My name is {greeting} </Text>
+     
+    </View>
+  
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45,
+  },
+  zapataStyle: {
+    fontSize: 20,
+  }
+});
+
+export default ComponentsScreen;
